f7c98e9c9bda5e7053cc7249d1ae969e49d11784
chandan viswakarma <chandanvishw11002@gmail.com>
2024-06-13 21:02:59 +0530

    initialized

--- index.js
+++ index.js
@@ -20,8 +20,13 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  const unix = req.params.date;
+  res.json({
+    unix:unix,
+    utc: new Date().toString(),
+  });
+  // res.json({greeting: 'hello API'});
 });
 
 
